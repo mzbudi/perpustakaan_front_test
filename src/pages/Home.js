2d4c5e1c74cc148bd198b5fe6a279359ee8da1b3
MzBudi <ronibudianto361@gmail.com>
2020-03-11 17:39:37 +0700

    First Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from "react";
+
+class Home extends Component {
+  render() {
+    return <p>ini Halaman Home</p>;
+  }
+}
+
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from "react";
+
+class Login extends Component {
+  render() {
+    return <p>ini Halaman Login</p>;
+  }
+}
+
+export default Login;
